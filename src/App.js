a98817b6803afcf737487c10aa909667b6c46cb5
Jenessa <jenessaw31@gmail.com>
2016-12-03 15:02:40 -0600

    getting data working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+  getApiInfo() {
+    axios.get('http://localhost:3030/products?$select[]=name&$select[]=id&$select[]=model&$select[]=description&$select[]=image&$select[]=url&$select[]=price&$select[]=shipping&$sort[price]=-1&$limit=12')
+  .then(function (response) {
+    console.log(response.data.data[0].name);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.getApiInfo.bind(this)}>Get Data</button>
       </div>
     );
   }
