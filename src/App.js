9c10f50db54361f4b7b5875d38cd3eb72ea8dff1
Sarah Rose Battles <srosebattles@gmail.com>
2016-12-04 16:53:04 -0600

    beginning search function

--- src/App.js
+++ src/App.js
@@ -10,21 +10,45 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        inventory: this.props.inventory
+        inventory: this.props.inventory,
+        newItemValue: ''
       }
     }
 
     onDeleteClick(id, e) {
-      console.log("You clicked delete, my friend!", id);
 
       var confirmed = confirm("Do you want to permanently delete this product from the database?")
-      if (confirmed == true){
+      if (confirmed === true){
         console.log('http://localhost:3030/products/'+id)
         axios.delete('http://localhost:3030/products/'+id)
       } else {
         console.log("Whew!")
       }
 
+    }
+
+    onNewValue(e) {
+     this.setState({
+       newItemValue: e.target.value
+     });
+    }
+
+    getSearchedInfo(e){
+      e.preventDefault()
+      console.log(this.state.newItemValue)
+      axios.get('http://localhost:3030/products/'+ this.state.newItemValue)
+      .then((response) => {
+        console.log(response.data.data);
+        var newInventory = response.data.data.slice(0);
+        console.log(newInventory);
+        this.setState({
+          inventory: newInventory
+        })
+        console.log(this.state.inventory);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
 
   getApiInfo(e) {
@@ -53,6 +77,10 @@ class App extends Component {
         <p className="App-intro">
         Welcome, we hope you enjoy your shopping experience.
         </p>
+        <form onSubmit={this.getSearchedInfo.bind(this)}>
+        <input className="searchy" type="text" placeholder="enter product name" onChange={this.onNewValue.bind(this)} value={this.state.newItemValue}/>
+        <button>Search</button>
+        </form>
         <div>
         <List
           inventory={this.state.inventory}
